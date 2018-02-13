54638d45811167ed54ba185e760959819c033ad8
nnradovic <nnradovic@gmail.com>
2018-02-13 01:19:46 +0100

    Week2 Day2

---
+++ loops/loopFive.js
@@ -0,0 +1,10 @@
+var sum = 1;
+for(i=0; i<20; i++){
+
+    sum =+ i*i;
+
+}
+
+console.log(sum)
+
+
---
+++ loops/loopOne.js
@@ -0,0 +1,33 @@
+for (var i = 0; i <= 15; i++) {
+
+    if (i % 2 === 0) {
+        console.log(i + " number is even.");
+    } else {
+        console.log(i + " this number is odd.");
+    }
+}
+
+for (i = 0; i < 1000; i++) {
+
+
+    if (i % 3 === 0 && i % 5 === 0) {
+
+        console.log(i + " is devisible with three anf five")
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
